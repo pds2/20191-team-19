cdd0b1806b3f2b09e6268670f33a7417c45e8050
pedrovxm <48036934+pedrovxm@users.noreply.github.com>
2019-06-04 20:02:17 -0300

    Create pessoa.h

---
+++ include/pessoa.h
@@ -0,0 +1,18 @@
+#ifndef PESSOA_H
+#define PESSOA_H
+
+#include <string>
+#include "player.h"
+#include "mao.h"
+
+class Pessoa: public Player{
+    public:
+        Pessoa(string nome);
+        virtual void pedirTruco();
+        virtual bool revisarTruco();
+        virtual Mao get_mao();
+        virtual void receberMao(Mao mao);
+        void jogarCoberta();
+};
+
+#endif
