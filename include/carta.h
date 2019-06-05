d65fe0b0b274a8fc26cd4f03f6b7db62a185617d
bcosta98 <49317441+bcosta98@users.noreply.github.com>
2019-06-04 21:05:03 -0300

    Create carta.h

---
+++ include/carta.h
@@ -0,0 +1,23 @@
+#ifndef CARTA_H
+#define CARTA_H
+
+#include <string>
+
+using namespace std;
+
+class Carta{
+    private:
+        string _nome;
+        int _peso;
+        string _naipe;
+        void geraNaipe();
+        void conferirManilha();
+        void geraNome(int peso);
+    public:
+        Carta();
+        string get_nome();
+        int get_peso();
+        string get_naipe();
+};
+
+#endif
