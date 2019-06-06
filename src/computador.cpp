a55c2e819d21bafed45a2442c81beab88f76e43e
Bruno Costa <brunooc.bc@gmail.com>
2019-06-06 04:05:48 +0000

    Update

--- src/computador.cpp
+++ src/computador.cpp
@@ -34,7 +34,17 @@ void Computador::jogar(Mesa m){
             }
     }
     if(this->_time == 2 && posicao == 0 || posicao == 2){
-
+        list<Carta>::iterator it;
+        int menor = INT16_MAX;
+        string carta;
+        for (it = this->_mao._cartas.begin(); it != this->_mao._cartas.end(); it++)
+        {
+            if(it->get_peso() < menor){
+                menor = it->get_peso();
+                carta = it->get_nome() + " " + it->get_naipe(); //MEXER AQUI
+            }
+        }
+        cout << carta;
     }
     
 }
