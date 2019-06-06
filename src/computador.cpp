18298e0a1b4b76dcbdaddfb6e05a0b68cff8612d
bcosta98 <49317441+bcosta98@users.noreply.github.com>
2019-06-06 11:56:50 -0300

    Update computador.cpp

--- src/computador.cpp
+++ src/computador.cpp
@@ -27,24 +27,30 @@ Mao Computador::get_mao(){
 void Computador::jogar(Mesa m){
     int posicao = 0;
     int maior = -1;
+    //Procura pela maior carta na mesa
     for (int i = 0; i < 4; i++){
             if(m.cartas[i] > maior){
                 maior = m.cartas[i];
                 posicao = i;
             }
     }
+    /*Verifica se o computador que está jogando é do time 2(Computadores) 
+    e se a maior carta é de um deles*/
     if(this->_time == 2 && posicao == 0 || posicao == 2){
         list<Carta>::iterator it;
         int menor = INT16_MAX;
         string carta;
         for (it = this->_mao._cartas.begin(); it != this->_mao._cartas.end(); it++)
         {
+            //Procura a menor carta para jogar
             if(it->get_peso() < menor){
                 menor = it->get_peso();
                 carta = it->get_nome() + " " + it->get_naipe(); //MEXER AQUI
             }
         }
+        //Imprime e adiciona na mesa
         cout << carta;
+        if(posicao == 2) m[0] = menor : m[2] = menor;
     }
     
 }
