e1850c073eb4791cc789fef2e17fe5c912898781
bcosta98 <49317441+bcosta98@users.noreply.github.com>
2019-06-20 19:40:42 -0300

    Update main.cpp

--- bin/main.cpp
+++ bin/main.cpp
@@ -4,16 +4,13 @@
 #include <list>
 #include <ctime>
 #include <cstdlib>
-#include "../include/carta.h"
-#include "../include/mao.h"
-#include "../include/computador.h"
 #include "../include/jogo.h"
-#include "../include/pessoa.h"
 
 using namespace std;
 int main()
 {
     srand((unsigned)time(NULL));
+    system("clear");
     /*
         NAIPES:
         "♥"
@@ -35,14 +32,6 @@ int main()
             ESPADILHA = 12;
             7 COPAS = 13;
             ZAP = 14;
-
-    Mao m = Mao();
-    list<Carta> :: iterator it;
-    for(it = m._cartas.begin(); it != m._cartas.end(); it++){
-        cout << "Carta:" << it->get_nome() << " " << it->get_naipe() << endl;
-        cout << "Valor:" << it->get_peso() << endl;
-    }
-    Computador c = Computador("PC 1", m, 1);
     */
 
     int opcao;
@@ -54,89 +43,16 @@ int main()
     cout << " 1 - Iniciar Jogo" << endl;
     cout << " 2 - Carregar Jogo" << endl;
     cout << " 3 - Sair" << endl;
-    cout << ">";
+    cout << " >";
     cin >> opcao;
 
     //Inicia o jogo atraves da opcao desejada
     Jogo j = Jogo(opcao);
-
-    //Cria a mesa
-    Mesa mesa = Mesa();
-
-    //Cria o jogador
-    Pessoa p = Pessoa(j.get_nome());
-
-    //Cria os computadores
-    Computador c1 = Computador("PC 1", 2, 1);
-    Computador c2 = Computador("PC 2", 1, 2);
-    Computador c3 = Computador("PC 3", 2, 3);
-
-    //Cria as maos
-    Mao m = Mao(mesa);
-    p.receberMao(m);
-
-    m = Mao(mesa);
-    c1.receberMao(m);
-    cout << "MAO C1" << endl;
-    c1.imprime_mao();
-
-    cout << "" << endl;
-    cout << "" << endl;
-
-    m = Mao(mesa);
-    c2.receberMao(m);
-    cout << "MAO C2" << endl;
-    c2.imprime_mao();
-
-    cout << "" << endl;
-    cout << "" << endl;
-
-    m = Mao(mesa);
-    c3.receberMao(m);
-    cout << "MAO C3" << endl;
-    c3.imprime_mao();
-
-    cout << "" << endl;
-    cout << "" << endl;
-    int i = 0;
-    while(i < 3){
-        c1.jogar(mesa);
-        mesa.imprime_mesa(p.get_nome(), c1.get_nome(), c2.get_nome(), c3.get_nome());
-        cout << endl;
-        c2.jogar(mesa);
-        mesa.imprime_mesa(p.get_nome(), c1.get_nome(), c2.get_nome(), c3.get_nome());
-        cout << endl;
-        c3.jogar(mesa);
-        mesa.imprime_mesa(p.get_nome(), c1.get_nome(), c2.get_nome(), c3.get_nome());
-        cout << endl;
-
-        Carta c = p.escolhe_carta();
-        //mesa.imprime_mesa(p.get_nome(), c1.get_nome(), c2.get_nome(), c3.get_nome(), c);
-
-        cout << endl;
-        c1.imprime_mao();
-        c2.imprime_mao();
-        c3.imprime_mao();
-        p.imprime_mao();
-        cout << endl;
-
-        int vencedor = mesa.verificaQuemGanhou();
-        if (vencedor == 0 || vencedor == 2)
-            j.set_pntTimeA(mesa.valorRodada);
-        else
-            j.set_pntTimeB(mesa.valorRodada);
-        mesa.limpaMesa();
-        i++;
+    int ganhou = j.iniciarJogo();
+    if(time){
+        cout << "PARABÉNS, VOCÊ GANHOU!!";
+    }else{
+        cout << "TENTE NOVAMENTE MAIS TARDE";
     }
-    /*
-    p.imprime_mao();
-    //escolhe a primeira carta
-    p.escolhe_carta(1);
-    p.imprime_mao();
-    p.escolhe_carta(1);
-    p.imprime_mao();
-    p.escolhe_carta(1);
-    p.imprime_mao();
-    */
     return 0;
 }
